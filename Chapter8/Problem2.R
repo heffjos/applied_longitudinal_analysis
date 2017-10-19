e700b3b745a85022c63f51ee26a14a99101f853c
Joe Heffernan <heffjos@umich.edu>
2017-10-19 12:45:33 -0500

    Updated Problem2 in chapter8

--- Chapter8/Problem2.R
+++ Chapter8/Problem2.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(foreign)
 library(nlme)
+library(modelr)
+library(broom)
+library(forcats)
 
 # id
 # group
@@ -47,9 +50,79 @@ cd4 <- cd4 %>%
     )
   )
 
-model1 <- lme(
+# model1 <- lme(
+#   logcd4 ~ week + week16 + week:group.factor + week16:group.factor,
+#   data = cd4,
+#   random = ~ week + week16 | id
+# )
+
+# 8.2.3
+# Is a model with only randomly varying intercepts defensible? Explain?
+model2 <- lme(
   logcd4 ~ week + week16 + week:group.factor + week16:group.factor,
   data = cd4,
-  random = ~ week + week16 + week:group.factor + week16:group.factor | id
+  random = ~ 1 | id
 )
+
+insufficient <- cd4 %>%
+  count(id) %>% 
+  filter(n < 4)
+
+sufficient <- anti_join(cd4, insufficient, by = "id")
+
+week_model <- function(df) {
+  lm(logcd4 ~ week + week16, data = df)
+}
+
+# sufficient <- sufficient %>%
+#   group_by(id) %>%
+#   nest() %>%
+#   mutate(
+#     lm_model = map(data, week_model),
+#     data = map2(data, lm_model, add_predictions),
+#     coefficients = map(lm_model, tidy)
+#   ) %>%
+#   unnest(coefficients) %>%
+#   left_join(select(cd4, id, group) %>% distinct(), by = "id")
+
+int_data <- sufficient %>%
+  filter(term == "(Intercept)") %>%
+  mutate(id_factor = fct_reorder2(factor(id), group, estimate))
+
+p_int2 = int_data %>%
+  ggplot(aes(x = estimate, y = id_factor)) + 
+  geom_point() +
+  geom_errorbarh(aes(xmax = estimate + std.error, xmin = estimate - std.error)) +
+  facet_grid(group ~ .)
+
+p_week = sufficient %>%
+  filter(term == "week") %>%
+  mutate(id_factor = fct_reorder2(factor(id), group, estimate)) %>%
+  ggplot(aes(x = estimate, y = id_factor)) + 
+  geom_point() +
+  geom_errorbarh(aes(xmax = estimate + std.error, xmin = estimate - std.error)) +
+  facet_grid(group ~ .)
+
+p_week16 = sufficient %>%
+  filter(term == "week16") %>%
+  mutate(id_factor = fct_reorder2(factor(id), group, estimate)) %>%
+  ggplot(aes(x = estimate, y = id_factor)) + 
+  geom_point() +
+  geom_errorbarh(aes(xmax = estimate + std.error, xmin = estimate - std.error)) +
+  facet_grid(group ~ .)
+
+# cd4_groups <- groupedData(
+#   logcd4 ~ week + week16 | id,
+#   data = as_data_frame(sufficient),
+#   labels = list(x = "week", y = "logcd4"),
+#   outer = ~ group
+# )  
+
+# No it is not defensible. According to AIC, BIC, and logLike, model1 is better.
+# Also looking at the plots (p_week and p_week16) there is inidvidual varaion among the
+# models which is better explained by a model with random intercepts and slopes.
+
+# 8.2.4
+# Construct a 6-degress-of-freedom test of the null hypthesis of no treatment
+# group differences in the changes in log CD4 counts.
 
